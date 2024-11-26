f51b381ce382df0b635a9442f09ebcec292f13b5
mac <shaydullinayrat@gmail.com>
2024-11-27 00:32:34 +0300

    hello world

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  '0.0.0.0',
