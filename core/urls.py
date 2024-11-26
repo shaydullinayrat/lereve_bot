77ed95c39d54e538eb2b46c90e6332956252be72
mac <shaydullinayrat@gmail.com>
2024-11-26 23:33:54 +0300

    hello world

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
-
+def hello_world(request):
+    return HttpResponse("Hello, World!")
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', hello_world, name='hello_world'),
 ]
