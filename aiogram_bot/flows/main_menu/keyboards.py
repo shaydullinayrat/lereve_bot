f2e993df8e08ef3982177bff9d8e06fcdabfeba4
mac <shaydullinayrat@gmail.com>
2024-11-28 12:41:23 +0300

    shop url in main page

--- aiogram_bot/flows/main_menu/keyboards.py
+++ aiogram_bot/flows/main_menu/keyboards.py
@@ -1,9 +1,9 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 from aiogram_bot.keyboards import generate_keyboard
 
 
-def start_keyboard():
+def start_keyboard_old():
     buttons_data = [
         ("Получить бонус", "bonus"),
         ("Наш магазин", "shops"),
@@ -12,6 +12,23 @@ def start_keyboard():
     ]
     layout = [2, 2]
     return generate_keyboard(buttons_data, layout)
+
+
+def start_keyboard():
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Получить бонус", callback_data="bonus"),
+                InlineKeyboardButton(text="Наш магазин", url="https://www.wildberries.ru/brands/le-reve/duhi"),
+            ],
+            [
+                InlineKeyboardButton(text="Служба заботы", callback_data="care_service"),
+                InlineKeyboardButton(text="Как пользоваться", callback_data="instructions"),
+            ],
+        ]
+    )
+    return keyboard
+
 
 
 def back_to_main_menu_keyboard():
