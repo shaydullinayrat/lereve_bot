9b204ff176c8db9fd17cbd8217d62f6a7cc5ca0d
mac <shaydullinayrat@gmail.com>
2024-11-29 22:54:33 +0300

    care service addiitonal

--- aiogram_bot/utils.py
+++ aiogram_bot/utils.py
@@ -4,7 +4,7 @@ from aiogram.types import Message
 
 def get_client_name(message):
     first_name = message.from_user.first_name
-    client_name = 'Дорогой клиент'
+    client_name = 'Дорогой покупатель'
     if first_name and 'Ле Реве' not in first_name and 'Le Reve' not in first_name:
         client_name = f'{first_name}'
     return client_name
