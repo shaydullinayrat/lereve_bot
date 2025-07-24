4e761d7e39abd595f620b28f303b11e694104304
shaydullinayrat <shaydullinayrat@gmail.com>
2025-07-24 12:49:08 +0300

    Update settings.py for improved configuration management

--- core/settings.py
+++ core/settings.py
@@ -162,18 +162,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-# Относительный путь для хранения собранных статических файлов
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# Абсолютный путь для хранения собранных статических файлов (production)
+STATIC_ROOT = '/webapps/lereve_bot/static/'
+MEDIA_ROOT = '/webapps/lereve_bot/media/'
 
-# Дополнительно: пользовательские директории со статическими файлами
+# Дополнительно: пользовательские директории со статическими файлами (оставляем для разработки)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'), 
-    # os.path.join(BASE_DIR, 'staticfiles/'),  # Например, директория для пользовательской статики
+    os.path.join(BASE_DIR, 'static/'),
+    # os.path.join(BASE_DIR, 'staticfiles/'),
 ]
 
 # Default primary key field type
