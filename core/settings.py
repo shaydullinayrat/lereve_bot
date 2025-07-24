e651fa0a80aad37c43ca030674b13f796b83eed6
shaydullinayrat <shaydullinayrat@gmail.com>
2025-07-24 13:40:14 +0300

    ff

--- core/settings.py
+++ core/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 
 # Относительный путь для хранения собранных статических файлов
